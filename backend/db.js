0f420a14efbb2377a590ceebfafffe75f5a3c59a
Yazid dz06 <yazidkhoualdi5@gmail.com>
2025-09-11 15:56:41 +0100

    render

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,13 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  }
-);
-
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false }, // nécessaire si Render exige SSL
+  },
+});
 
 (async () => {
   try {
